8354cdd18e8728f4af72c50aa0be14d0416ded51
push.default <manrevlob@us.es>
2015-11-30 16:22:51 +0100

    Modificadas Tablas de la BBDD
    Modificadas las tablas Votes y Votacion, añadiendo los correspondientes DROP en caso de que ya existieran.

--- BD/SQL.sql
+++ BD/SQL.sql
@@ -1,15 +1,19 @@
-CREATE TABLE IF NOT EXISTS `Votacion` (
-  `votacion_id`  int(11) NOT NULL,
-  `name` text NOT NULL,
-   PRIMARY KEY (votacion_id)
+DROP TABLE IF EXISTS `Votes`;
+DROP TABLE IF EXISTS `Votacion`;
+
+CREATE TABLE `Votacion` (
+	`votacion_id`  int(11) NOT NULL AUTO_INCREMENT,
+	`name` text NOT NULL,
+	PRIMARY KEY (votacion_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `Votes` (
-  `vote_id` int(11) NOT NULL,
+CREATE TABLE `Votes` (
+  `vote_id` int(11) NOT NULL AUTO_INCREMENT,
   `vote` text NOT NULL,
   `votation_id` int(11) NOT NULL,
   `latitud` double NOT NULL,
   `longitud` double NOT NULL,
+  PRIMARY KEY (vote_id),
   FOREIGN KEY (votation_id) 
         REFERENCES Votacion(votacion_id)
         ON DELETE CASCADE
