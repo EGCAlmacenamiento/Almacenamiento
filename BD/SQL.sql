9e0140afeaf60a517d74c0a5f13c35e8456b8b16
push.default <manrevlob@us.es>
2015-11-30 16:25:38 +0100

    Añadido Procedimiento
    Añadido procedimiento al fichero SQL para insertar un voto

--- BD/SQL.sql
+++ BD/SQL.sql
@@ -18,3 +18,32 @@ CREATE TABLE `Votes` (
         REFERENCES Votacion(votacion_id)
         ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Procedimiento Creacion Votacion y Voto
+DROP PROCEDURE IF EXISTS `InsertarVotacionVoto`;
+
+DELIMITER //
+
+CREATE PROCEDURE InsertarVotacionVoto
+(
+	IN votacionName VARCHAR(100), 
+	IN voto VARCHAR(100), 
+	IN latitud double, 
+	IN longitud double
+)
+BEGIN
+	DECLARE votacionId INT DEFAULT null;
+
+	SELECT votacion_id INTO votacionId FROM Votacion WHERE name like votacionName;
+	IF votacionId = null THEN
+		INSERT INTO Votacion VALUES(votacionName);
+		SELECT LAST_INSERT_ID() as votId FROM Votacion;
+		INSERT INTO Votes values(voto, votId, latitud, longitud);
+	ELSE
+		SELECT LAST_INSERT_ID() as votId FROM Votacion;
+		INSERT INTO Votes values(voto, votId, latitud, longitud);
+    END IF;
+END
+
+//
+DELIMITER ;
