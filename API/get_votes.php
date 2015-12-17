b440b2899a05115cdf3abf5a61d60e388d5c28ca
fravalpin <fravalpin@alum.us.es>
2015-12-17 16:55:49 +0100

    Modificado get_votes.php
    Ya devuelve un JSON con los votos perfectamente

--- API/get_votes.php
+++ API/get_votes.php
@@ -19,8 +19,15 @@ if ($votation == 0 || $conn->connect_error) {
 $sql = "SELECT vote FROM Votes WHERE votation_id = '".$votation."'";
 $result = $conn->query($sql);
 
-$row = $result->fetch_assoc();
-echo $row;
+$votes = array();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $votes[] = $row["vote"];
+    }
+}
+
+//print_r( array_values($votes));
+echo json_encode($votes);
 
 
 
