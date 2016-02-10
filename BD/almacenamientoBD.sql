8432e5d0665dba64d42b6d40d6a20c08e6737681
Andres Lopez <andres.albin93@gmail.com>
2016-02-10 13:35:08 +0100

    Movido proyecto maven
    cambiada la carpeta de almacenamiento GUI

--- BD/almacenamientoBD.sql
+++
@@ -1,8 +0,0 @@
-drop database if exists `almacenamientobd`;
-create database `almacenamientobd`;
-
-grant select, insert, update, delete on `Getting-Started`.*to'acme-user'@'%';
-grant select, insert, update, delete, create, drop, references, index, alter,
- create temporary tables, lock tables, create view, create routine,
- alter routine, execute, trigger, show view
- on `almacenamientobd`.* to 'acme-manager'@'%';
